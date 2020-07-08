a8cf807f3c9ed9f138ef9d915e0c7f02d30e4929
Douglas Sousa <douglascamilo@gmail.com>
2020-07-07 22:55:02 -0300

    Recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,16 +6,17 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent {
+  @Output() recipeItemClicked = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Recipe 01', 'a test recipe - 01',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
     new Recipe('Recipe 02', 'a test recipe - 02',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+      'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg')
   ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onRecipeItemClicked(recipeItem: Recipe): void {
+    this.recipeItemClicked.emit(recipeItem);
   }
 }
