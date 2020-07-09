88e7abe6ccc2ed1d565e3e13a52fc9c0827a8163
Douglas Sousa <douglascamilo@gmail.com>
2020-07-09 02:11:38 -0300

    Dropdown directive improvements

--- src/app/shared/directives/dropdown.directive.ts
+++ src/app/shared/directives/dropdown.directive.ts
@@ -5,24 +5,31 @@ import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular
 })
 export class DropdownDirective implements OnInit {
   private BOOTSTRAP_SHOW_CLASS = 'show';
+  private BOOTSTRAP_DROPDOWN_MENU_CLASS = '.dropdown-menu';
   private dropdownMenu: HTMLElement;
-  private isOpen = false;
 
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    this.dropdownMenu = this.elementRef.nativeElement.querySelector('.dropdown-menu');
+    this.dropdownMenu = this.elementRef.nativeElement.querySelector(this.BOOTSTRAP_DROPDOWN_MENU_CLASS);
   }
 
-  @HostListener('click')
-  onClick(): void {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event.target.parentElement'])
+  public onClick(parentElement: HTMLElement): void {
+    const shouldOpen = this.shouldOpenDropdownMenu(parentElement);
     this.renderer.removeClass(this.dropdownMenu, this.BOOTSTRAP_SHOW_CLASS);
 
-    if (this.isOpen) {
+    if (shouldOpen) {
       this.renderer.addClass(this.dropdownMenu, this.BOOTSTRAP_SHOW_CLASS);
     }
+  }
+
+  private shouldOpenDropdownMenu(parentElement: HTMLElement): boolean {
+    const dropdownMenuElement: HTMLElement = parentElement?.isEqualNode(this.elementRef.nativeElement) ?
+      parentElement.querySelector(this.BOOTSTRAP_DROPDOWN_MENU_CLASS) : null;
+
+    return dropdownMenuElement ? !dropdownMenuElement.classList.contains(this.BOOTSTRAP_SHOW_CLASS) : false;
   }
 }
