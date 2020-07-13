1852233b80d35d858d308bea895b6310604af533
Douglas Sousa <douglascamilo@gmail.com>
2020-07-13 16:36:16 -0300

    Unsubscribing to params observable

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
+  private paramsSubscription: Subscription;
   user: {id: number, name: string};
 
   constructor(
@@ -19,10 +21,14 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
 
-    this.route.params
+    this.paramsSubscription = this.route.params
       .subscribe((params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
       });
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 }
