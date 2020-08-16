73f55622661d607af69de8a6fb0387f59320833b
Douglas Sousa <douglascamilo@gmail.com>
2020-08-16 19:16:23 -0300

    Sending a POST request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,16 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
     this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+      .post('https://ng-complete-guide-bb985.firebaseio.com/posts.json', postData)
+      .subscribe(response => console.log(response));
   }
 
   onFetchPosts() {
