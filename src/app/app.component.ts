dc53065cba6184775b5b7a7da74e8fb1146a9688
Douglas Sousa <douglascamilo@gmail.com>
2020-08-16 19:25:16 -0300

    Getting data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,35 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  private readonly POSTS_ENDPOINT = 'https://ng-complete-guide-bb985.firebaseio.com/posts.json';
   loadedPosts = [];
 
   constructor(
     private http: HttpClient
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
 
   onCreatePost(postData: { title: string; content: string }) {
     this.http
-      .post('https://ng-complete-guide-bb985.firebaseio.com/posts.json', postData)
+      .post(this.POSTS_ENDPOINT, postData)
       .subscribe(response => console.log(response));
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(this.POSTS_ENDPOINT)
+      .subscribe(posts => console.log(posts));
   }
 }
