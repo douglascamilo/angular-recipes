16b1cf48b2a70130761ca03558691c14e60ffac7
Douglas Sousa <douglascamilo@gmail.com>
2020-08-19 00:03:50 -0300

    Storing recipes on Firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
+  private readonly URL = 'https://course-recipe-book-20db2.firebaseio.com/recipes.json';
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private recipeService: RecipeService
+  ) {
+  }
+
+  storeRecipes(): void {
+    const recipes = this.recipeService.getRecipes();
+
+    this.http
+      .put(this.URL, recipes)
+      .subscribe(response => console.log(response));
+  }
 }
